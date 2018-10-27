d5a38d432a43b7d1329d311afd05ff02d4e2cf12
EitanGIP <eitan.kosman@gmail.com>
2018-10-27 11:32:19 +0300

    edit controller

--- controller/controller.py
+++ controller/controller.py
@@ -1,4 +1,5 @@
 import pyvjoy
+import keyboard
 
 current_x = 0x4000 # wheel
 current_y = 0x4000 # accelerate
@@ -6,7 +7,9 @@ current_z = 0x4000 # brake
 
 
 def register_device():
-    #acquire device
+    '''
+    acquire device
+    '''
     j = pyvjoy.VJoyDevice(1)
     if j is None:
         return False
@@ -16,33 +19,53 @@ def register_device():
     current_y = 0x4000  # accelerate
     current_z = 0x4000  # brake
 
+    print("Reset axis X")
     j.set_axis(pyvjoy.HID_USAGE_X, current_x)
-    j.set_axis(pyvjoy., current_x)
-    j.set_axis(pyvjoy.HID_USAGE_X, current_x)
+    print("Reset axis Y")
+    j.set_axis(pyvjoy.HID_USAGE_Y, current_y)
+    print("Reset axis Z")
+    j.set_axis(pyvjoy.HID_USAGE_Z, current_z)
 
     # Notice the args are (buttonID,state) whereas vJoy's native API is the other way around.
-    for i in range(10): # turn all buttons off
-        j.set_button(i,0)
+    for i in range(1, 9):  # turn all buttons off
+        print("Reset button", i)
+        j.set_button(i, 0)
 
     return j
 
 
+def update_position(axis, value, device):
+    if axis == 'x':
+        device.set_axis(pyvjoy.HID_USAGE_X, value)
+    elif axis == 'y':
+        device.set_axis(pyvjoy.HID_USAGE_Y, value)
+    elif axis == 'z':
+        device.set_axis(pyvjoy.HID_USAGE_Z, value)
 
 
+def press_button(device, button):
+    device.set_button(button,1)
 
 
+def release_button(device, button):
+    device.set_button(button,0)
 
 
-import keyboard #Using module keyboard
-while True:#making a loop
-    try: #used try so that if user pressed other than the given key error will not be shown
-        if keyboard.is_pressed(keyboard.KEY_DOWN):#if key 'q' is pressed
-            if current_pos > 0x1:
-                current_pos -= 1
-                j.set_axis(pyvjoy.HID_USAGE_X, current_pos)
-        elif keyboard.is_pressed(keyboard.KEY_UP):
-            if current_pos < 0x8000:
-                current_pos += 1
-                j.set_axis(pyvjoy.HID_USAGE_X, current_pos)
-    except:
-        break #if user pressed a key other than the given key the loop will break
+if __name__ == '__main__':
+    j = register_device()
+    if j is None:
+        print("failed to register device")
+        exit(0)
+
+    while True:
+        try:
+            if keyboard.is_pressed(keyboard.KEY_DOWN):
+                if current_y > 0x1:
+                    current_y -= 1
+                    j.set_axis(pyvjoy.HID_USAGE_Z, current_y)
+            elif keyboard.is_pressed(keyboard.KEY_UP):
+                if current_y < 0x8000:
+                    current_y += 1
+                    j.set_axis(pyvjoy.HID_USAGE_Z, current_y)
+        except:
+            break
